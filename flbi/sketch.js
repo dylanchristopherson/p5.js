1b7c65ceefd3d910daaa8a4a9a3304325fc5acae
dylanchristopherson <dchristropherson@gmail.com>
2018-07-16 17:19:21 -0600

    Circle follows mouse now

--- flbi/sketch.js
+++ flbi/sketch.js
@@ -1,7 +1,31 @@
+var c;
+
 function setup() {
   // put setup code here
+  createCanvas(400,400);
+  c = new Circle();
 }
 
 function draw() {
   // put drawing code here
+  // clear();   
+
+  background(51);               
+  c.update();
+  c.show();
+}
+
+function Circle() {
+  this.x = mouseX;
+  this.y = mouseY;
+
+  this.update = function() {
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+ 
+ this.show = function() {
+    ellipse(this.x, this.y, 10, 10);
+  }
+
 }
