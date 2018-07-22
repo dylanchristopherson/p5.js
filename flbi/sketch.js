f985ab5c67ede89116b2af0832c8e6a281ce70c6
dylanchristopherson <dchristropherson@gmail.com>
2018-07-22 17:19:44 +0000

    Watching video to help improve gravity

--- flbi/sketch.js
+++ flbi/sketch.js
@@ -1,6 +1,7 @@
 var c;
 var WIDTH = 400;
-var HEIGHT = 400;
+var HEIGHT = 400
+
 
 function setup() {
   // put setup code here
@@ -21,35 +22,39 @@ function draw() {
 function Circle() {
   this.x = 150;    // x position
   this.y = 10;     // y position
-  this.g = .1;     // gravity
-  this.a = 0;
+  this.gravity = .1;     // gravity
+  this.velocity = 0;    // velocity
+  this.lift = -10;
 
   this.update = function() {
     
-    this.g = this.g + (this.g * .1);
-    this.y = this.y + 2 + this.g;
-   
+    this.velocity += this.gravity; 
+    this.y += this.velocity;
+ 
  
  // this.y = mouseY;
     if(this.y >= HEIGHT) {
       this.y = HEIGHT;
+      this.velocity = 0;
     }
     if(this.y <= 0) {
       this.y = 0;
+      this.velocity = 0;
     }
   }
  
   this.show = function() {
-    ellipse(this.x, this.y, 25, 15);
+    ellipse(this.x, this.y, 30, 30);
   }
 
   this.up = function() {
-    this.y = this.y - 100;
-    this.g = .1;
+    this.velocity += this.lift;
   }
 
 }
 
 function keyPressed() {
-  c.up();  
+  if (key == ' ') {  
+    c.up();  
+  }
 }
